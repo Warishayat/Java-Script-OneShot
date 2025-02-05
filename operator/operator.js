2ee08d62a9877d294154d649150eabd98a314b11
Waris Hayat <122077609+Warishayat@users.noreply.github.com>
2025-02-05 16:43:27 +0500

    operators etc

---
+++ operator/operator.js
@@ -0,0 +1,35 @@
+// Conditional operators 
+let age = 19;
+if (age>18)
+{
+    console.log("Successs")
+}
+else{
+    console.log("Not Success")
+}
+
+// operator and  or 
+let num = 18;
+let iseducation = false;
+
+if (num>=18 && iseducation==true){
+    console.log("Successs")
+}
+else{
+    console.log("Not yed success")
+}
+
+
+// Asignment-operators
+let num1 = 12;
+let num2 = 14;
+
+let addition =  num1+num2;
+let division = num1/num2;
+let multiplication = num1 * num2;
+let minus =  num1 -  num2;
+
+console.log("Addition is:",+addition);
+console.log("Addition is:",+division);
+console.log("Addition is:",+multiplication);
+console.log("Addition is:",+minus);
