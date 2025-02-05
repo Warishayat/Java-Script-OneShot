55cb7cc61f36899f68f766ce3998a707a895216b
Waris Hayat <122077609+Warishayat@users.noreply.github.com>
2025-02-05 16:45:00 +0500

    While/For -loops

---
+++ loops/loops.js
@@ -0,0 +1,16 @@
+// While loops it take conf=dition update and initialization
+let i =1;
+
+while(i<10){
+    console.log(i);
+    i+=1;
+}
+console.log("Loop end here");
+
+
+
+// for loops it take conf=dition update and initialization to make the while loop easy
+for (let n=1;n<=10;n++){
+    console.log(n);
+}
+console.log("Looops end here guys")
