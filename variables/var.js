8923781cf00adb95ca8e8cf7c3e941fcd1142dc7
Waris Hayat <122077609+Warishayat@users.noreply.github.com>
2025-02-05 16:42:53 +0500

    variables etc

---
+++ variables/var.js
@@ -0,0 +1,25 @@
+// the most used way that used in the industry
+let z =10; 
+let x =20;
+
+
+let sum = z+x;
+
+console.log("The result of a and b is:"+sum)
+
+
+
+// once const is devined itcant be chnage
+const  a= 30;
+console.log("a HAS THE value which is const:"+a)
+
+
+
+//  var which is old method that is depricated in the js
+var c = 11;
+var b =23;
+
+var multiplicatioon = c*b;
+
+
+console.log("The multiplication result is:"+multiplicatioon)
