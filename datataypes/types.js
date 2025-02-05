ae8c822efc5637c34c206006b49ed7afa4a3ffc6
Waris Hayat <122077609+Warishayat@users.noreply.github.com>
2025-02-05 16:45:43 +0500

    Primitive/ Non-primitive

---
+++ datataypes/types.js
@@ -0,0 +1,49 @@
+//  Thera are two types of data in the javascripy
+
+//  1: Primitive  Data Types
+//  Under Primitive data types we may found string boolean undefined and nulls etc
+
+// 2: Non primitive data types
+//  Under Non Primitive dta type we have Array objects etc
+
+let name = null;
+console.log(name);
+
+
+let name1 = undefined;
+console.log(name1);
+
+// let a=10;
+// console.log(typeof(a));
+
+// let b ="waris"
+// console.log(typeof(b));
+
+// let isString = true ; 
+// console.log(typeof(isString));
+
+
+// //  let solve a simple question to calulcate the radius
+
+// const pie =3.14;
+// let radius = 12;
+
+// let area = pie * radius **2;
+
+// console.log("area is:"+area)
+
+// let parameter = 2 * pie * radius **2;
+// console.log("PARAMATER:"+parameter)
+
+
+
+// // short hand assignemnet variables
+// let x = 13;
+// x+=10;
+// console.log(x)
+// x-=12;
+// console.log(x)
+// x/=2;
+// console.log(x)
+// x*=2;
+// console.log(x)
